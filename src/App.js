1f15dbd09a557c95ae570e4991eac0d255ba499a
raghavgupta4475 <raghavkumargupta18@gmail.com>
2019-07-22 14:49:02 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './views/LoginPage/LoginPage';
+import OptionsPage from './views/OptionsPage/OptionsPage';
+
+function App() {
+  return (
+    <div>
+
+    <LoginPage/>
+    {/* <OptionsPage/> */}
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/views/OptionsPage/OptionsPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from 'components/Header/Header.jsx'
+const options =() =>{
+    return(
+        <div>
+            <Header
+            absolute 
+            color="Blue"
+            brand="IC ordering Appliation"
+            />
+        </div>
+
+    );
+
+}
+
+export default options;
